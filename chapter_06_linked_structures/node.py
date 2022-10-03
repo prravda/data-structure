a4944f0ffea8796ae1b30243add0feaf08cd7dfe
prravda <sonofhuman20@hufs.ac.kr>
2022-10-03 09:18:44 +0900

    Chore - removed unused file   `stack_using_node.py` - set python package structure   with creating `__init__.py` - changed property name and remove   useless code lines from `node.py`

--- chapter_06_linked_structures/node.py
+++ chapter_06_linked_structures/node.py
@@ -1,22 +1,7 @@
 from typing import Optional
 
 
-# set node's next link to next node
 class Node:
-    def __init__(self, val: Optional[int] = None, link: Optional["Node"] = None):
-        self.val = val
+    def __init__(self, elem: Optional[int] = None, link: Optional["Node"] = None):
+        self.data = elem
         self.link = link
-
-
-# singly linked list
-singly_linked_list = Node(1)
-singly_linked_list.link = Node(2)
-singly_linked_list.link.link = Node(3)
-
-# circular linked list
-circular_linked_list = Node(1)
-circular_linked_list.link = Node(2)
-circular_linked_list.link.link = Node(3)
-circular_linked_list.link.link.link = circular_linked_list
-
-# TODO: implement doubly linked list with inheriting the Node class
