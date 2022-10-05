8f28009d33685c248f4f2604f07516f58040da2c
prravda <sonofhuman20@hufs.ac.kr>
2022-10-05 22:21:22 +0900

    Refactored - update the type of Node's link property   from `Node` to `Optional["Node"]`

--- chapter_06_linked_structures/node.py
+++ chapter_06_linked_structures/node.py
@@ -4,4 +4,4 @@ from typing import Optional, Any
 class Node:
     def __init__(self, elem: Optional[int] = None, link: Optional["Node"] = None):
         self.data: Any = elem
-        self.link: Node = link
+        self.link: Optional["Node"] = link
