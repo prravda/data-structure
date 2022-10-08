037f32f4a427cf53892853ad2a1b7f671ab81725
prravda <sonofhuman20@hufs.ac.kr>
2022-10-08 20:32:35 +0900

    Refactor - change the constructor of `node.py`   - from `elem`to `data`

--- chapter_06_linked_structures/node.py
+++ chapter_06_linked_structures/node.py
@@ -2,6 +2,6 @@ from typing import Optional, Any
 
 
 class Node:
-    def __init__(self, elem: Optional[int] = None, link: Optional["Node"] = None):
-        self.data: Any = elem
+    def __init__(self, data: int, link: Optional["Node"] = None):
+        self.data: int = data
         self.link: Optional["Node"] = link
